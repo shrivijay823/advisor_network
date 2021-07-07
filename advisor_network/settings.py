282b60aa31855113a0cbd4b7efe1f5ba89838349
shrivijay823 <shrivijay823@gmail.com>
2021-07-07 11:06:47 +0530

    additional files and changes for Heroku

--- advisor_network/settings.py
+++ advisor_network/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'qu7n5$r%^3y8m3st@ds(ipuzsn--g@jx%k7fyvhy_o3e7+5jfj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
